12d3665316750742c709516eb688496c83a2ef39
Douglas Moraes <douglas.am@gmail.com>
2025-06-15 17:43:39 -0400

    Initial project scaffold

---
+++ src/main/java/com/dam/starwars/StarwarsApiApplication.java
@@ -0,0 +1,13 @@
+package com.dam.starwars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarwarsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarwarsApiApplication.class, args);
+	}
+
+}
